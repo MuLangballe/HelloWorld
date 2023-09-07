0eb727896efc2fad95b995a7907920967e79ab6e
MuLangballe <mala0007@stud.kea.dk>
2023-09-07 21:17:45 +0200

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+import java.time.LocalDateTime;
+
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World\nHello dat23D");
+        System.out.println("Dette er årstal: "+ 23+1);
+        LocalDateTime todayTime = LocalDateTime.now();
+        System.out.println(todayTime);
+        System.out.println(todayTime);
+
+        System.out.println(LocalDateTime.now());
+        System.out.println(LocalDateTime.now());
+        System.out.println(LocalDateTime.now());
+
+    }
+}
